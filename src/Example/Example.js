73ec0766ee507f129c0053de7f1e522161bbbf2d
neaguolt <neaguolt@gmail.com>
2023-05-10 20:44:24 +0300

    Render inputs with map and list and div

--- src/Example/Example.js
+++ src/Example/Example.js
@@ -1,25 +1,39 @@
 import React, { useState } from 'react';
 
 export default function Example() {
-  const [user, setUser] = useState({
-    name: 'John',
-    age: 30,
-    email: 'john@example.com'
-  });
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUser({
-      ...user, // se copiaza intregul obiect de stare
-      [name]: value // se actualizeaza proprietatea corespunzatoare
+    const [user, setUser] = useState({
+      name: 'John',
+      age: 30,
+      email: 'john@example.com'
     });
+  
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setUser({
+        ...user,
+        [name]: value
+      });
+    }
+  
+    const inputFields = [
+      { name: 'name', label: 'Name', type: 'text' },
+      { name: 'age', label: 'Age', type: 'number' },
+      { name: 'email', label: 'Email', type: 'email' }
+    ];
+  
+    return (
+      <div>
+        {inputFields.map((field) => (
+          <div key={field.name}>
+            <label htmlFor={field.name}>{field.label}</label>
+            <input
+              type={field.type}
+              name={field.name}
+              value={user[field.name]}
+              onChange={handleInputChange}
+            />
+          </div>
+        ))}
+      </div>
+    );
   }
-
-  return (
-    <div>
-      <input type="text" name="name" value={user.name} onChange={handleInputChange} />
-      <input type="number" name="age" value={user.age} onChange={handleInputChange} />
-      <input type="email" name="email" value={user.email} onChange={handleInputChange} />
-    </div>
-  );
-}
