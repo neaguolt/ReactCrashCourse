a39462e6f4650a0a21cf07009edea0490500f57a
neaguolt <neaguolt@gmail.com>
2023-05-18 16:01:57 +0300

    Key Unique Siblings -Blog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 //import './App.css';
 
-import ListElements from './Example/ListElements';
+import Blog from "./Example/Blog";
 
 const App = () => {
   return (
-    <ListElements numbers= {[1,2,3]} />
+    <Blog posts={[
+      {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+      {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+    ]}/>
   );
 }
 export default App;
---
+++ src/Example/Blog.js
@@ -0,0 +1,27 @@
+//Key do not have to be unique only among siblings
+export default function Blog (props) {
+    const sidebar = (
+        <ul>
+            {
+                props.posts.map((post) =>
+                    <li key= {post.id}>
+                        {post.title}
+                    </li>
+                )
+            }
+        </ul>
+    );
+    const content = props.posts.map((post) =>
+            <div key= {post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.content}</p>
+            </div>
+    );
+    return (
+        <div>
+            {sidebar}
+            <hr />
+            {content}
+        </div>
+    )
+}
