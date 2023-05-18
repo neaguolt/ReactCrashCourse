9cb3ab353ac3d1215a06f05762e2bc0d262ca3a8
neaguolt <neaguolt@gmail.com>
2023-05-18 21:46:04 +0300

    Key is not transmiten on components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 //import './App.css';
 
-import Blog from "./Example/Blog";
+import Post from "./Example/Post";
 
+const posts = [
+  { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
+  { id: 2, title: 'Installation', content: 'You can install React from npm.' }
+];
+const content= posts.map((post) => 
+  <Post key={post.id}
+        id={post.id}    
+        title={post.title}>
+  </Post>
+);
 const App = () => {
+  
   return (
-    <Blog posts={[
-      {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
-      {id: 2, title: 'Installation', content: 'You can install React from npm.'}
-    ]}/>
-  );
+    <div>{content}</div> 
+  )
 }
 export default App;
---
+++ src/Example/Post.js
@@ -0,0 +1,5 @@
+export default function Post(props) {
+    return (
+        <h3>{props.title}</h3>
+    )
+}
